b563afb17addf5f50f03fe9cc2dc37d15ce1e178
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-10-26 14:34:28 -0700

    Added some needed things

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const canvas = document.getElementById('gameArea')
 const ctx = canvas.getContext('2d')
 
+function startGame() {
+    document.getElementById('startMenu').style.display = 'none'
+    canvas.style.display = 'block'
+    drawButton(healthBar)
+}
+
+let healthBar = {
+    x: 50,
+    y: 50,
+    width: 80,
+    height: 20,
+    color: "red"
+}
+
+function drawButton(name) {
+    ctx.fillStyle = name.color;
+    ctx.fillRect(name.x,name.y,name.width,name.height)
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 4
+    ctx.strokeRect(name.x, name.y, name.width, name.height)
+}
+
