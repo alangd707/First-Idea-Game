5f0d259babd78872d88aa262293ad43ac98ccf20
alangd707 <alangd707@west-mec.org>
2022-10-27 18:29:34 -0700

    almostdone

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-const canvas = document.getElementById('gameArea')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('gameArea');
+const ctx = canvas.getContext('2d');
+const imgFolder = document.getElementById('item-storage')
+const createImg = document.createElement('img')
+// Where all the objects will be stored for future use.
+
+let testDummy = {
+    imgFile: "./img/testDummy.png",
+    HP: 100,
+    DPS: 5
+}
 
 function startGame() {
-    document.getElementById('startMenu').style.display = 'none'
-    canvas.style.display = 'block'
+    document.getElementById('startMenu').style.display = 'none';
+    canvas.style.display = 'block';
     fightDummy();
 }
 
 function fightDummy() {
-    let x = canvas.width.x
-    let y = canvas.width.y
     // Starting change of graphics
     ctx.strokeStyle = "white";
     ctx.lineWidth = 5;
     ctx.lineCap = 'square';
 
     ctx.beginPath();
-    ctx.moveTo(0, 400);
-    ctx.lineTo(800, 400);
+    ctx.moveTo(0, 300);
+    ctx.lineTo(800, 300);
     ctx.stroke();
 
-    ctx.strokeRect(266., 330, 267, 50);
+    ctx.strokeRect(300, 350, 200, 50);
 
-    // Variables of objects used
-    let healthBar = {x: 0, y: 0, width: 80, height: 20, color: "red"};
-    let testDummy = {width: 200, height: 50};
-}
+    drawMob(testDummy)
 
-let healthBar = {
-    x: 0,
-    y: 0,
-    width: 80,
-    height: 20,
-    color: "red"
-}
-
-function drawButton(name) {
-    ctx.fillStyle = name.color;
-    ctx.fillRect(name.x,name.y,name.width,name.height)
+    function drawMob(mobID) {
+        createImg.src = mobID.imgFile
+        createImg.id = 'mob-img'
+        imgFolder.appendChild(createImg)
+        ctx.drawImage(document.getElementById('mob-img'), 300, 75, 200, 150)
+    }
 }
